9cd93f6a1e3663935d3b171f6166e986d0f3e1e9
Hammad <muhammadhammaduddin1@gmail.com>
2025-02-12 03:15:11 +0500

    Initial commit: Angular project for webpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: 'Home' },
+  { path: 'about', component: AboutComponent, title: 'About' },
+  { path: '**', component: NotFoundComponent },
+];
